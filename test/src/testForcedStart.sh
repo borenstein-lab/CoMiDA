739d83346e8c27ea27f58beb2cdfb5e4d1ce7a21
Alex Eng <engal@optonline.net>
2015-07-22 16:18:49 -0700

    Updated the test scripts to use the new force substrates flag

--- test/src/testForcedStart.sh
+++ test/src/testForcedStart.sh
@@ -2,6 +2,6 @@
 BIN_DIR=(BIN_DIR)
 TEST_DIR=(TEST_DIR)
 
-$BIN_DIR/design_community -f 01 $TEST_DIR/data/input/testForcedStart.edges 01 02 > testForcedStart.species
+$BIN_DIR/design_community -fs 01 $TEST_DIR/data/input/testForcedStart.edges 01 02 > testForcedStart.species
 $BIN_DIR/check_output $TEST_DIR/data/output/testForcedStart.out testForcedStart.species
 rm testForcedStart.species
